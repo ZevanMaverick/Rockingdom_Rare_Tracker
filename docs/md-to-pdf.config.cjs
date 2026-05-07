37078c06b942d5240dd8469ab1a84508286d7a98
Rockingdom <rockingdom@example.com>
2026-05-07 22:53:04 +0800

    Add PDF exports

---
+++ docs/md-to-pdf.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  launch_options: {
+    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+  },
+  pdf_options: {
+    format: 'A4',
+    printBackground: true,
+    margin: {
+      top: '16mm',
+      right: '14mm',
+      bottom: '16mm',
+      left: '14mm',
+    },
+  },
+}
+
